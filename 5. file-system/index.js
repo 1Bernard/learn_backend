8180465a473528651ff3544bcd5c45c28527d0ac
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2025-05-01 07:08:01 +0000

    `Added asynchronous file operations using fs.writeFile, fs.readFile, and fs.appendFile in index.js`

--- 5. file-system/index.js
+++ 5. file-system/index.js
@@ -18,3 +18,26 @@ console.log("File content:", readContentFromFile);
 
 fs.appendFileSync(filePath, '\nHello from node js again');
 console.log('new file content added successfully');
+
+//asynchronous way of creating the file
+const asyncFilePath = path.join(dataFolder, 'async-example.txt');
+fs.writeFile(asyncFilePath, 'Hello Async node js', (err) => {
+  if (err) throw err;
+  console.log('Async file created successfully');
+
+  fs.readFile(asyncFilePath, 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log("Async file content:", data);
+
+    fs.appendFile(asyncFilePath, '\nThis is another line added', (err) => {
+      if (err) throw err;
+      console.log('New line added to Async file');
+    })
+
+    fs.readFile(asyncFilePath, 'utf-8', (err, updatedData) => {
+      if (err) throw err;
+      console.log("Updated file content:", data);
+    })
+  })
+
+})
